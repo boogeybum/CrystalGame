92fac39820d3125763f69c0b2ac26988f2ef38d6
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-02-17 01:00:42 -0600

    Revised the function setTarget for setting the random target number with min and max number values. Placed button tags around the apprentice image. Added a random number value function that currently outputs to the playerTally id with min max values of 1 and 10 respectively. This will be for randomly assigning values to the images that when clicked will add to the playerTally total.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,22 @@
-var targetNumber = Math.floor(Math.random() * 100)
+// var targetNumber = Math.floor(Math.random() * 100);
+// var imageNumber = Math.floor(Math.random() * 100);
+
 
 function setTarget() {
+    var minNumber = 20;
+    var maxNumber = 50;
+    var targetNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
     $("#targetNum").text("Target Number: " + targetNumber);
 }
 
+
+function imageValue() {
+    var minNumber = 1;
+    var maxNumber = 10;
+    var imageNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    $("#playerScore").text("Player's Score: " + imageNumber);
+    // $("#apprentice").attr(imageNumber);
+}
+
 setTarget();
+imageValue();
